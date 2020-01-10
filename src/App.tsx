e20096192c8b692b6f79879a949b869fb97e6ee6
alex-kostenko <0984181760ss@gmail.com>
2020-01-10 17:36:21 +0200

    git init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, ChangeEvent} from 'react';
+import {Task} from './modal/task';
 import './App.css';
 
+var data = [ 
+  { _id: 0, done: false, text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, ratione. '},
+  { _id: 1, done: false, text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, ratione. '},
+  { _id: 2, done: false, text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, ratione. '}
+];
+
+localStorage.setItem('toDoList', JSON.stringify(data));
+
+
 const App: React.FC = () => {
+
+  useEffect(() => {
+    const storage = localStorage.getItem('toDoList')
+    if (storage) {
+      const data = JSON.parse(storage)
+      setToDoList(data)
+    }
+  }, [])
+
+  const [toDoList, setToDoList] = useState<Task[]>([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, item: Task) => {
+    let data = toDoList.concat();
+    let currentItem = item;
+    currentItem.done = e.target.checked;
+    data[item._id] = currentItem;
+    setToDoList(data)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {toDoList.map((item: Task) => 
+        <div key={item._id}>
+          <input type="checkbox" checked={item.done} onChange={(e) => handleChange(e, item)}/>
+          {item.text}
+        </div>
+      )}
     </div>
   );
 }
